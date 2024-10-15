4901cbe89fdeb7bcc9a91d02b617e6a9f2b5acb4
Volodymyr Porokhniak <volodymyr.porokhniak@cybericebox.com>
2024-10-15 23:52:50 +0300

    Remove swagger

--- internal/delivery/controller/http/handler/handler.go
+++ internal/delivery/controller/http/handler/handler.go
@@ -1,16 +1,11 @@
 package handler
 
 import (
-	"fmt"
-	"github.com/cybericebox/daemon/internal/config"
 	"github.com/cybericebox/daemon/internal/delivery/controller/http/handler/auth"
-	"github.com/cybericebox/daemon/internal/delivery/controller/http/handler/docs"
 	"github.com/cybericebox/daemon/internal/delivery/controller/http/handler/event"
 	"github.com/cybericebox/daemon/internal/delivery/controller/http/handler/exercise"
 	"github.com/cybericebox/daemon/internal/delivery/controller/http/handler/user"
 	"github.com/gin-gonic/gin"
-	swaggerFiles "github.com/swaggo/files"
-	ginSwagger "github.com/swaggo/gin-swagger"
 )
 
 type (
@@ -26,12 +21,6 @@ type (
 	}
 )
 
-// @title           Cyber ICE Box Platform API
-// @version         1.0
-// @description     This is the API for the Cyber ICE Box Platform
-
-// @BasePath  /api
-
 func NewAPIHandler(useCase IUseCase) *Handler {
 	return &Handler{useCase: useCase}
 }
@@ -39,10 +28,6 @@ func NewAPIHandler(useCase IUseCase) *Handler {
 func (h *Handler) Init(router *gin.Engine) {
 	baseAPI := router.Group("api", corsMiddleware)
 	{
-		// swagger docs
-		docs.SwaggerInfo.Host = fmt.Sprintf("%s://%s", config.SchemeHTTPS, config.PlatformDomain)
-		baseAPI.GET("/docs/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
-
 		auth.NewAuthAPIHandler(h.useCase).Init(baseAPI)
 		event.NewEventAPIHandler(h.useCase).Init(baseAPI)
 		exercise.NewExerciseAPIHandler(h.useCase).Init(baseAPI) // all routes are protected
