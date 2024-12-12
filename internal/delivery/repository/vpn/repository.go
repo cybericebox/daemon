6d8b4b0c4d8c276332c458c2df85020442a19da5
Volodymyr Porokhniak <volodymyr.porokhniak@cybericebox.com>
2024-12-12 22:08:44 +0200

    update vpn method on new wireguard service version

--- internal/delivery/repository/vpn/repository.go
+++ internal/delivery/repository/vpn/repository.go
@@ -6,12 +6,14 @@ import (
 	"github.com/cybericebox/daemon/internal/model"
 	"github.com/cybericebox/wireguard/pkg/controller/grpc/client"
 	"github.com/cybericebox/wireguard/pkg/controller/grpc/protobuf"
+	"github.com/gofrs/uuid"
 	"github.com/rs/zerolog/log"
+	"time"
 )
 
 type (
 	VPNRepository struct {
-		protobuf.WireguardClient
+		client client.WireguardClient
 	}
 
 	Dependencies struct {
@@ -31,7 +33,7 @@ func NewRepository(deps Dependencies) *VPNRepository {
 	}
 }
 
-func newVPN(cfg *config.VPNGRPCConfig) (protobuf.WireguardClient, error) {
+func newVPN(cfg *config.VPNGRPCConfig) (client.WireguardClient, error) {
 	c, err := client.NewWireguardConnection(client.Config{
 		Endpoint: cfg.Endpoint,
 		Auth: client.Auth{
@@ -45,36 +47,86 @@ func newVPN(cfg *config.VPNGRPCConfig) (protobuf.WireguardClient, error) {
 		},
 	})
 	if err != nil {
-		return nil, model.ErrVPN.WithError(err).WithMessage("Failed to create VPN client").Cause()
+		return nil, model.ErrVPN.WithError(err).WithMessage("Failed to create VPN client").Err()
 	}
 
-	//if _, err = c.Ping(context.Background(), &protobuf.EmptyRequest{}); err != nil {
-	//	return nil, model.ErrVPN.WithError(err).WithMessage("Failed to ping VPN").Cause()
-	//}
+	if _, err = c.Ping(context.Background(), &protobuf.EmptyRequest{}); err != nil {
+		return nil, model.ErrVPN.WithError(err).WithMessage("Failed to ping VPN").Err()
+	}
 
 	return c, nil
 }
 
-func (r *VPNRepository) GetVPNClientConfig(ctx context.Context, userID, groupID, destCIDR string) (string, error) {
-	resp, err := r.WireguardClient.GetClientConfig(ctx, &protobuf.ClientConfigRequest{
-		UserID:   userID,
-		GroupID:  groupID,
+func (r *VPNRepository) GetVPNClients(ctx context.Context, userID, groupID uuid.UUID) ([]*model.VPNClient, error) {
+	resp, err := r.client.GetClients(ctx, &protobuf.ClientsRequest{
+		UserID:  userID.String(),
+		GroupID: groupID.String(),
+	})
+	if err != nil {
+		return nil, model.ErrVPN.WithError(err).WithMessage("Failed to get clients").WithContext("userID", userID).WithContext("groupID", groupID).Err()
+	}
+
+	clients := make([]*model.VPNClient, 0, len(resp.GetClients()))
+	for _, c := range resp.GetClients() {
+		clients = append(clients, &model.VPNClient{
+			UserID:   uuid.FromStringOrNil(c.GetUserID()),
+			GroupID:  uuid.FromStringOrNil(c.GetGroupID()),
+			Banned:   c.GetBanned(),
+			LastSeen: time.Unix(c.GetLastSeen(), 0),
+		})
+	}
+
+	return clients, nil
+}
+
+func (r *VPNRepository) GetVPNClientConfig(ctx context.Context, userID, groupID uuid.UUID, destCIDR string) (string, error) {
+	resp, err := r.client.GetClientConfig(ctx, &protobuf.ClientConfigRequest{
+		UserID:   userID.String(),
+		GroupID:  groupID.String(),
 		DestCIDR: destCIDR,
 	})
 	if err != nil {
-		return "", model.ErrVPN.WithError(err).WithMessage("Failed to get client config").WithContext("userID", userID).WithContext("groupID", groupID).WithContext("destCIDR", destCIDR).Cause()
+		return "", model.ErrVPN.WithError(err).WithMessage("Failed to get client config").WithContext("userID", userID).WithContext("groupID", groupID).WithContext("destCIDR", destCIDR).Err()
 	}
 
 	return resp.GetConfig(), nil
 }
 
-func (r *VPNRepository) DeleteVPNClients(ctx context.Context, userID, groupID string) error {
-	if _, err := r.WireguardClient.DeleteClients(ctx, &protobuf.ClientsRequest{
-		UserID:  userID,
-		GroupID: groupID,
+func (r *VPNRepository) DeleteVPNClients(ctx context.Context, userID, groupID uuid.UUID) error {
+	if _, err := r.client.DeleteClients(ctx, &protobuf.ClientsRequest{
+		UserID:  userID.String(),
+		GroupID: groupID.String(),
 	}); err != nil {
-		return model.ErrVPN.WithError(err).WithMessage("Failed to delete client").WithContext("userID", userID).WithContext("groupID", groupID).Cause()
+		return model.ErrVPN.WithError(err).WithMessage("Failed to delete client").WithContext("userID", userID).WithContext("groupID", groupID).Err()
 	}
 
 	return nil
+}
+
+func (r *VPNRepository) BanVPNClients(ctx context.Context, userID, groupID uuid.UUID) error {
+	if _, err := r.client.BanClients(ctx, &protobuf.ClientsRequest{
+		UserID:  userID.String(),
+		GroupID: groupID.String(),
+	}); err != nil {
+		return model.ErrVPN.WithError(err).WithMessage("Failed to ban client").WithContext("userID", userID).WithContext("groupID", groupID).Err()
+	}
+
+	return nil
+}
+
+func (r *VPNRepository) UnBanVPNClients(ctx context.Context, userID, groupID uuid.UUID) error {
+	if _, err := r.client.UnBanClients(ctx, &protobuf.ClientsRequest{
+		UserID:  userID.String(),
+		GroupID: groupID.String(),
+	}); err != nil {
+		return model.ErrVPN.WithError(err).WithMessage("Failed to unban client").WithContext("userID", userID).WithContext("groupID", groupID).Err()
+	}
+
+	return nil
+}
+
+func (r *VPNRepository) Close() {
+	if err := r.client.Close(); err != nil {
+		log.Error().Err(err).Msg("Failed to close VPN repository")
+	}
 }
